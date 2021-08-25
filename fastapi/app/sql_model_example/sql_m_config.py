9c0d2ee96dfb0fd4179e91ab1aa018622ba5a521
FourMoBro <FourMoBro@users.noreply.github.com>
2021-08-24 20:25:15 -0700

    models

---
+++ fastapi/app/sql_model_example/sql_m_config.py
@@ -0,0 +1,11 @@
+from sqlmodel import Session, SQLModel, create_engine
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+pg_url2 = os.environ.get("PG_URL")
+
+engine = create_engine(pg_url2)
+
+SessionLocal = Session(engine)
